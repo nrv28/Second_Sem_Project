a1880b1dd21f0a0592af44b977ab0f03ee948428
Kamran <akhtarkamran676@gmail.com>
2023-04-21 20:06:54 +0530

    Initializing Project

---
+++ mainfile.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+//Add functions for program here
+
+int main(){
+    cout<<"Welcome to ....\n"; //Enter name of program
+    cout<<"1.\n";
+    //Add your code here
+    cout<<"Select one of the above number : ";
+    int op;
+    cin>>op;
+    switch(op){
+        case 1:
+            //Enter function call here
+            break;
+        case 2:
+            
+            break;
+        default:
+            cout<<"Invalid Choice!"<<endl;
+            break;
+    }
+    
+    return 0;
+}
